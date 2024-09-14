cc44d3b9a7af3a27ad1a94b1c012b230b906c617
Robin CR <robin005cr@gmail.com>
2024-09-14 22:17:14 +0530

    adding the main and sln
    adding the main and sln

---
+++ repo/repo/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+struct trainingParam 
+{
+	int resLength, resBreadth, framePerSec;
+};
+struct cpuParam 
+{
+	int clockFrequency;
+};
+
+int main()
+{
+	
+
+	return 0;
+}
